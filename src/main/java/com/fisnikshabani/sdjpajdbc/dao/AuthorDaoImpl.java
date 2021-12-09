71a97d956ef217a0c18ddc0e17ba4721cd1cabfa
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-12-09 10:57:41 +0100

    Using prepared statement.

--- src/main/java/com/fisnikshabani/sdjpajdbc/dao/AuthorDaoImpl.java
+++ src/main/java/com/fisnikshabani/sdjpajdbc/dao/AuthorDaoImpl.java
@@ -4,10 +4,7 @@ import com.fisnikshabani.sdjpajdbc.domain.Author;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao{
@@ -22,13 +19,15 @@ public class AuthorDaoImpl implements AuthorDao{
     @Override
     public Author getById(Long id) {
         Connection connection = null;
-        Statement statement = null;
+        PreparedStatement ps = null;
         ResultSet resultSet = null;
 
         try {
             connection = source.getConnection();
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM author where id = " + id);
+            ps = connection.prepareStatement("SELECT * FROM author WHERE id = ?");
+            ps.setLong(1,id);
+            //resultSet = statement.executeQuery("SELECT * FROM author where id = " + id);
+            resultSet = ps.executeQuery();
 
             if (resultSet.next()){
                 Author author = new Author();
@@ -45,8 +44,8 @@ public class AuthorDaoImpl implements AuthorDao{
                 if (resultSet != null){
                     resultSet.close();
                 }
-                if (statement != null){
-                    statement.close();
+                if (ps != null){
+                    ps.close();
                 }
                 if (connection != null){
                     connection.close();
